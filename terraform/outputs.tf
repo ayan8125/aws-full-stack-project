79806bf436ab99c638fb3cc6f71a01066b1c195b
Ayan shaikh <44862892+ayan8125@users.noreply.github.com>
2026-03-13 13:29:16 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -18,3 +18,23 @@ output "database-endpoint" {
 output "cloudfront_url" {
   value = aws_cloudfront_distribution.frontend_cdn.domain_name
 }
+
+output "ECR_REPOSITORY" {
+  description = "Amazon ECR repository name where Docker images for the backend will be pushed"
+  value       = aws_ecr_repository.backend_repo.name
+}
+
+output "ECS_CLUSTER_NAME" {
+  description = "Name of the ECS cluster where backend containers are running"
+  value       = aws_ecs_cluster.main.name
+}
+
+output "ECS_SERVICE_NAME" {
+  description = "ECS service updated during deployment"
+  value       = aws_ecs_service.backend_service.name
+}
+
+output "S3_BUCKET_NAME" {
+  description = "S3 bucket hosting the frontend"
+  value       = aws_s3_bucket.frontend_bucket.bucket
+}
